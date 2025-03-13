42ce2f67199e7faa871ea7c3a8293ebe5155a0b3
egymatador2024 <egymatador2024@gmail.com>
2025-03-13 07:38:23 +0200

    update

--- src/app/components/Cv/page.tsx
+++ src/app/components/Cv/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 const Page = () => {
   return (
     <main className="flex justify-center items-center h-[150px] bg-gradient-to-br bg-purple-600 ">
-      <button 
-        type="button" 
-        className="bg-yellow-600 text-white font-semibold px-10 py-5 rounded-md shadow-md hover:bg-red-700 transition duration-300 flex items-center"
+      <button
+        type="button"
+        className="bg-gradient-to-r to-yellow-500 from-yellow-900 text-white font-semibold px-10 py-5 rounded-md shadow-md hover:from-yellow-600 hover:to-red-700 transition duration-300 flex items-center"
       >
-        Download CV <IoCloudDownloadOutline color='white'size={22}/>
+        Download CV <IoCloudDownloadOutline color="white" size={22} />
       </button>
     </main>
   );
